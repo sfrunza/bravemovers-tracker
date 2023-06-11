728e3c91bf7f40471c2911a8dbb6081ceafd14dd
sfrunza <frunza.sergiu3@gmail.com>
2023-06-11 17:21:23 -0400

    lock

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Jobsbook
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "http://localhost:3000, https://bravemovers-tracker-app.herokuapp.com"
+        origins "http://localhost:3000, https://bravemovers-tracker.herokuapp.com"
         resource "*", :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
